54e62c12877357be921bb8b4a98f94d58a94b275
Lucy Brychkova <Lucy.Brychkova@myob.com>
2021-05-17 17:46:37 +1200

    Started String Calculator Kata. 4 steps are done.

---
+++ BasicFlowers/StringCalc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicFlowers
+{public class TDDCalc
+    {
+        public int StringCalculator(string inputFromUser)
+        {
+            var whatWeReturn = 0;
+
+            string[] values = inputFromUser.Split(',');
+
+            foreach(var element in values)
+            {
+                int.TryParse(element, out var outputValue);
+                //var outputValue = Int32.Parse(element);
+                whatWeReturn += outputValue;
+            }
+
+            
+
+
+
+            //try letter,number,letter
+
+            return whatWeReturn;
+        }
+    }
+}
+
+
