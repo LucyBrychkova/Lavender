a9a6214f31c6cbce7436a0b8be25c6ae6a3158f9
Lucy Brychkova <Lucy.Brychkova@myob.com>
2021-05-31 17:17:36 +1200

    Attempting to fix step 5 - string equals itself - not working

--- BasicFlowers/StringCalc.cs
+++ BasicFlowers/StringCalc.cs
@@ -10,15 +10,22 @@ namespace BasicFlowers
         public int StringCalculator(string inputFromUser)
         {
             var whatWeReturn = 0;
-
-            if (Step5(inputFromUser))
+            string MakeTheChange = inputFromUser;
+            if (Step5(MakeTheChange))
             {
-                inputFromUser.Replace("-n", ",");
-                Console.WriteLine(inputFromUser);
+                //inputFromUser.Replace("\n", ",");
+
+                
+                MakeTheChange = MakeTheChange.Replace("\n", ",");
+
+                //inputFromUser.Replace("1", ",");//this does not work at all
+                //Console.WriteLine(inputFromUser);
+                Console.WriteLine(MakeTheChange);
             }
 
-            string[] values = inputFromUser.Split(',');
-
+            //string[] values = inputFromUser.Split(',');
+            string[] values = MakeTheChange.Split(',');
+            //Console.WriteLine(values);
             //char[] delims = new[] { '\r', '\n', ',' };
             //string[] values = inputFromUser.Split(delims);
 
@@ -26,6 +33,7 @@ namespace BasicFlowers
             //string[] values = Regex.Split(inputFromUser, "[,\r\n]");
             foreach (var element in values)
             {
+                Console.WriteLine(element);
                 int.TryParse(element, out var outputValue);
                 //var outputValue = Int32.Parse(element);
                 whatWeReturn += outputValue;
@@ -40,7 +48,7 @@ namespace BasicFlowers
         public static bool Step5(string SlashN)
         {
 
-            if (SlashN.IndexOf("-n") >= 0)
+            if (SlashN.IndexOf("\n") >= 0)
             {
                 //SlashN.Replace("\n", ",");
                 return true;
